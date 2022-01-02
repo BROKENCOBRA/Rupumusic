2c3ef593133bc346e162d876445b28759e1b1c1b
S4shiv <87891148+S4shiv@users.noreply.github.com>
2022-01-02 22:36:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,60 +1,49 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
 from config import BOT_NAME as bn
 from helpers.filters import other_filters2
-from time import time
-from datetime import datetime
-from helpers.decorators import authorized_users_only
-from config import BOT_USERNAME, ASSISTANT_USERNAME
-
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ("week", 60 * 60 * 24 * 7),
-    ("day", 60 ** 2 * 24),
-    ("hour", 60 ** 2),
-    ("min", 60),
-    ("sec", 1),
-)
-
-
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return "inf"
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
-    return ", ".join(parts)
 
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-        await message.reply_text(
-        f"""**Hey, I'm {bn} 🎀
-I Cᴀɴ Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ Gʀᴏᴜᴩ Vᴏɪᴄᴇ Cʜᴀᴛ. Dᴇᴠᴇʟᴏᴩᴇᴅ Bʏ [𝐃𝐞𝐂𝐨𝐝𝐞-𝐃𝐞𝐯𝐬](https://t.me/DeeCodeDevs).
-Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ Aɴᴅ Pʟᴀʏ Mᴜsɪᴄ Fʀᴇᴇʟʏ!**
+    
+    await message.reply_text(
+        f"""**Hᴇʏ ɪᴛs {bn}** \n
+**I ᴀᴍ ʟᴀᴢʏ Aʙᴏᴜᴛ ᴛʏᴘɪɴɢ sᴏᴍᴇᴛʜɪɴɢ ɴᴇᴡ..ɪᴛᴢ ᴀ ʙᴏᴛ ᴍᴀᴅᴇ ғᴏʀ ᴘʟᴀʏ ᴍᴜsɪᴄ ɪɴ Vᴄ.😈❣️
+Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ : [𝐒•4•𝐒𝐡𝐢𝐯](https://t.me/shivamdemon)**.
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐋𝐢𝐬𝐭🧰", url="https://telegra.ph/text-10-24")
+                        "💞 ᴏᴡɴᴇʀ ", text="Fɪɴᴅɪɴɢ ʀᴜᴘᴀ🥺😇")
                   ],[
                     InlineKeyboardButton(
-                       " 𝐒𝐮𝐩𝐩𝐨𝐫𝐭👿", url="https://t.me/DecodeSupport"
+                        "🔥Aɴʏ Pʀᴏʙʟᴇᴍ", url="https://t.me/shivamdemon"
                     ),
                     InlineKeyboardButton(
-                        "𝐔𝐩𝐝𝐚𝐭𝐞𝐬", url="https://t.me/DeecodeBots"
+                        "🐬 Gʀᴏᴜᴘ", url="https://t.me/Love_live_laughk"
                     )
-                ],[
+                ],[ 
                     InlineKeyboardButton(
-                        "➕ 𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩➕",
-                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-                    )
-                ]
+                        "Aᴅᴅ ᴍʏ ʙᴏᴛ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴅᴀʀʟɪɴɢ🤭", url=f"https://t.me/itzrupu_vcbot?startgroup=true"
+                    )]
             ]
         ),
      disable_web_page_preview=True
     )
+
+@Client.on_message(filters.command("alive") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""Bᴏᴛ ɪɴ Fᴏʀᴍ ʙᴀʙʏ 😈""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "😎 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/Shivamdemon")
+                ]
+            ]
+        )
+   )
+ 
